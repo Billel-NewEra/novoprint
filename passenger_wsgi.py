e3da4632ae19d8cd068cfe5e81ac7ee9f8de335a
Bil <biliro014@gmail.com>
2025-10-09 17:05:03 +0100

    update

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,6 @@
 import sys, os
 
-# Ajouter le dossier courant au chemin Python
+# Ajouter le dossier courant au chemin Python.
 sys.path.insert(0, os.path.dirname(__file__))
 
 # Importer l'application Flask
